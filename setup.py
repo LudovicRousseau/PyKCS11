385fa79e8c4541eb27317831ef835385b0063fc5
Giuseppe Amato <gam@bit4id.com>
2006-09-01 12:02:49 +0000

    - added /usr/local include and lib in search paths (needed by FreeBSD) - compiles and runs on FreeBSD 6.1 (more testing needed) - all tabs converted to spaces

--- setup.py
+++ setup.py
@@ -3,19 +3,27 @@
 
 from distutils.core import setup, Extension
 from sys import version_info as pyver
+from os import path
+lib_dirs = []
+inc_dirs = ["src"]
+# some OS, such as FreeBSD, uses /usr/local folders
+if path.exists("/usr/local"):
+    lib_dirs.append("/usr/local/lib")
+    inc_dirs.append("/usr/local/include")
 
 setup(name="PyKCS11",
     version="1.1.0",
-	ext_modules=[
-		Extension(
-			"PyKCS11/_LowLevel",
-			["src/ck_attribute_smart.cpp", "src/pkcs11lib.cpp",
-			"src/pykcs11string.cpp", "src/unix_pykcs11_wrap.cpp",
-			"src/utility.cpp", "src/dyn_unix.c"],
-			include_dirs = ["src"],
-			libraries = ["python%d.%d" % pyver[:2]]
-		)
-	],
-	py_modules=["PyKCS11/__init__", "PyKCS11/LowLevel"],
+    ext_modules=[
+        Extension(
+            "PyKCS11/_LowLevel",
+            ["src/ck_attribute_smart.cpp", "src/pkcs11lib.cpp",
+            "src/pykcs11string.cpp", "src/unix_pykcs11_wrap.cpp",
+            "src/utility.cpp", "src/dyn_unix.c"],
+            include_dirs = inc_dirs,
+            library_dirs = lib_dirs,
+            libraries = ["python%d.%d" % pyver[:2]]
+        )
+    ],
+    py_modules=["PyKCS11/__init__", "PyKCS11/LowLevel"],
 )
 
