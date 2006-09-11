098e1aa165bada0be72ef0155c725b4f6f5ff961
Giuseppe Amato <gam@bit4id.com>
2006-09-11 13:26:52 +0000

    setup.py: 1) now works on win32, can generate binaries distribution packages (zip, installer); 2) detailed project description has been added

--- setup.py
+++ setup.py
@@ -4,26 +4,70 @@
 from distutils.core import setup, Extension
 from sys import version_info as pyver
 from os import path
+import platform
+
+description = '''A complete PKCS#11 wrapper for Python.
+You can use any PKCS#11 (aka CryptoKi) module such as the PSM which comes
+as part of mozilla or the various modules supplied by vendors of hardware
+crypto tokens, and almost all PKCS#11 functions and data types. The wrapper
+has been generated with the help of the SWIG compiler.'''
+
+classifiers = ["Development Status :: 5 - Production/Stable",
+"Intended Audience :: Developers",
+"License :: OSI Approved :: GNU General Public License (GPL)",
+"Natural Language :: English",
+"Operating System :: Microsoft :: Windows",
+"Operating System :: OS Independent",
+"Operating System :: Unix",
+"Programming Language :: C",
+"Programming Language :: C++",
+"Programming Language :: Python",
+"Topic :: Security :: Cryptography",
+"Topic :: Software Development :: Libraries :: Python Modules"]
+
 lib_dirs = []
 inc_dirs = ["src"]
 # some OS, such as FreeBSD, uses /usr/local folders
 if path.exists("/usr/local"):
     lib_dirs.append("/usr/local/lib")
     inc_dirs.append("/usr/local/include")
+source_files = ["src/ck_attribute_smart.cpp",
+                "src/pkcs11lib.cpp",
+                "src/pykcs11string.cpp",
+                "src/utility.cpp",
+                "src/pykcs11.cpp"]
+                
+if (platform.system().lower() == 'windows'):
+    source_files.append("src/dyn_win32.c")
+    source_files.append("pykcs11.rc")
+    source_files.append("src/win32_pykcs11_wrap.cpp")
+    libraries_val = ["python%d%d" % pyver[:2]]
+else:
+    source_files.append("src/dyn_unix.c")
+    source_files.append("src/unix_pykcs11_wrap.cpp")
+    libraries_val = ["python%d.%d" % pyver[:2]]
 
 setup(name="PyKCS11",
     version="1.1.0",
+    description="A Full PKCS#11 wrapper for Pyton",
+    keywords="crypto,pki,pkcs11,c++",
+    classifiers=classifiers,
+    platforms="Win32 Unix",
+    long_description=description,
+    author="Giuseppe Amato (Midori)",
+    author_email="paipai at tiscali.it",
+    url="http://www.bit4id.org/trac/pykcs11",
+    download_url="http://www.bit4id.org/trac/pykcs11/wiki/Download",
+    license="GPL",
     ext_modules=[
         Extension(
-            "PyKCS11/_LowLevel",
-            ["src/ck_attribute_smart.cpp", "src/pkcs11lib.cpp",
-            "src/pykcs11string.cpp", "src/unix_pykcs11_wrap.cpp",
-            "src/utility.cpp", "src/dyn_unix.c"],
+            "PyKCS11._LowLevel",
+            sources=source_files,
             include_dirs = inc_dirs,
             library_dirs = lib_dirs,
-            libraries = ["python%d.%d" % pyver[:2]]
+            libraries = libraries_val,
         )
     ],
-    py_modules=["PyKCS11/__init__", "PyKCS11/LowLevel"],
+    py_modules=["PyKCS11.__init__", "PyKCS11.LowLevel"],
 )
 
