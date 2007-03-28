68aff35c542813e22df90e4c19e3574ac589e449
lrousseau <lrousseau@localhost>
2007-03-28 13:36:31 +0000

    use a try: block to not stop on the first slot with an error

--- samples/dumpit.py
+++ samples/dumpit.py
@@ -98,128 +98,132 @@ slots = pkcs11.getSlotList()
 print "Available Slots:", len(slots)
 
 for s in slots:
-    i = pkcs11.getSlotInfo(s)
-    print "Slot no:", s
-    print format_normal % ("slotDescription", i.slotDescription.strip())
-    print format_normal % ("manufacturerID", i.manufacturerID.strip())
+    try:
+        i = pkcs11.getSlotInfo(s)
+        print "Slot no:", s
+        print format_normal % ("slotDescription", i.slotDescription.strip())
+        print format_normal % ("manufacturerID", i.manufacturerID.strip())
 
-    t = pkcs11.getTokenInfo(s)
-    print "TokenInfo"
-    print format_normal % ("label", t.label.strip())
-    print format_normal % ("manufacturerID", t.manufacturerID.strip())
-    print format_normal % ("model", t.model.strip())
+        t = pkcs11.getTokenInfo(s)
+        print "TokenInfo"
+        print format_normal % ("label", t.label.strip())
+        print format_normal % ("manufacturerID", t.manufacturerID.strip())
+        print format_normal % ("model", t.model.strip())
 
-    session = pkcs11.openSession(s)
-    print "Opened session 0x%08X" % session.session.value()
-    if pin_available:
-        try:
-            session.login(pin = pin)
-        except:
-            print "login failed, exception:", str(sys.exc_info()[1])
+        session = pkcs11.openSession(s)
+        print "Opened session 0x%08X" % session.session.value()
+        if pin_available:
+            try:
+                session.login(pin = pin)
+            except:
+                print "login failed, exception:", str(sys.exc_info()[1])
 
-    objects = session.findObjects()
-    print
-    print "Found %d objects: %s" % (len(objects), objects)
-
-    all_attributes = PyKCS11.CKA.keys()
-    # remove the CKR_ATTRIBUTE_SENSITIVE attributes since we can't get
-    # their values and will get an exception instead
-    all_attributes.remove(PyKCS11.CKA_PRIVATE_EXPONENT)
-    all_attributes.remove(PyKCS11.CKA_PRIME_1)
-    all_attributes.remove(PyKCS11.CKA_PRIME_2)
-    all_attributes.remove(PyKCS11.CKA_EXPONENT_1)
-    all_attributes.remove(PyKCS11.CKA_EXPONENT_2)
-    all_attributes.remove(PyKCS11.CKA_COEFFICIENT)
-
-    for o in objects:
+        objects = session.findObjects()
         print
-        print (red + "==================== Object: 0x%08X ====================" + normal) % o.value()
-        attributes = session.getAttributeValue(o, all_attributes)
-        attrDict = dict(zip(all_attributes, attributes))
-        if attrDict[PyKCS11.CKA_CLASS] == PyKCS11.CKO_PRIVATE_KEY \
-           and attrDict[PyKCS11.CKA_KEY_TYPE] == PyKCS11.CKK_RSA:
-            m = attrDict[PyKCS11.CKA_MODULUS]
-            e = attrDict[PyKCS11.CKA_PUBLIC_EXPONENT]
-            if m and e:
-                mx = eval('0x%s' % ''.join(chr(c) for c in m).encode('hex'))
-                ex = eval('0x%s' % ''.join(chr(c) for c in e).encode('hex'))
-            if sign:
-                try:
-                    toSign="12345678901234567890" # 20 bytes, SHA1 digest
-                    print "* Signing with object 0x%08X following data: %s" % (o.value(), toSign)
-                    signature = session.sign(o, toSign) 
-                    s = ''.join(chr(c) for c in signature).encode('hex')
-                    sx = eval('0x%s' % s)
-                    print "Signature:"
-                    print dump(''.join(map(chr, signature)), 16)
-                    if m and e:
-                        print "Verifying using following public key:"
-                        print "Modulus:"
-                        print dump(''.join(map(chr, m)), 16)
-                        print "Exponent:"
-                        print dump(''.join(map(chr, e)), 16)
-                        decrypted = pow(sx, ex, mx) # RSA
-                        if toSign == hexx(decrypted).decode('hex')[-20:]:
-                            print "*** signature VERIFIED!\n"
-                        else:
-                            print "*** signature NOT VERIFIED; decrypted value:"
-                            print hex(decrypted), "\n"
-                    else:
-                        print "Unable to verify signature: MODULUS/PUBLIC_EXP not found"
-                except:
-                    print "Sign failed, exception:", str(sys.exc_info()[1])
-            if decrypt:
+        print "Found %d objects: %s" % (len(objects), objects)
+
+        all_attributes = PyKCS11.CKA.keys()
+        # remove the CKR_ATTRIBUTE_SENSITIVE attributes since we can't get
+        # their values and will get an exception instead
+        all_attributes.remove(PyKCS11.CKA_PRIVATE_EXPONENT)
+        all_attributes.remove(PyKCS11.CKA_PRIME_1)
+        all_attributes.remove(PyKCS11.CKA_PRIME_2)
+        all_attributes.remove(PyKCS11.CKA_EXPONENT_1)
+        all_attributes.remove(PyKCS11.CKA_EXPONENT_2)
+        all_attributes.remove(PyKCS11.CKA_COEFFICIENT)
+
+        for o in objects:
+            print
+            print (red + "==================== Object: 0x%08X ====================" + normal) % o.value()
+            attributes = session.getAttributeValue(o, all_attributes)
+            attrDict = dict(zip(all_attributes, attributes))
+            if attrDict[PyKCS11.CKA_CLASS] == PyKCS11.CKO_PRIVATE_KEY \
+               and attrDict[PyKCS11.CKA_KEY_TYPE] == PyKCS11.CKK_RSA:
+                m = attrDict[PyKCS11.CKA_MODULUS]
+                e = attrDict[PyKCS11.CKA_PUBLIC_EXPONENT]
                 if m and e:
+                    mx = eval('0x%s' % ''.join(chr(c) for c in m).encode('hex'))
+                    ex = eval('0x%s' % ''.join(chr(c) for c in e).encode('hex'))
+                if sign:
                     try:
-                        toEncrypt="12345678901234567890"
-                        # note: PKCS1 BT2 padding should be random data,
-                        # but this is just a test and we use 0xFF...
-                        padded = "\x02%s\x00%s" % ("\xFF" * (128 - (len(toEncrypt)) -2), toEncrypt)
-                        print "* Decrypting with 0x%08X following data: %s" % (o.value(), toEncrypt)
-                        print "padded:\n",  dump(padded, 16)
-                        encrypted = pow(eval('0x%sL' % padded.encode('hex')), ex, mx) # RSA
-                        encrypted1 = hexx(encrypted).decode('hex')
-                        print "encrypted:\n", dump(encrypted1, 16)
-                        decrypted = session.decrypt(o, encrypted1)
-                        decrypted1 = ''.join(chr(i) for i in decrypted)
-                        print "decrypted:\n", dump(decrypted1, 16)
-                        if decrypted1 == toEncrypt:
-                            print "decryption SUCCESSFULL!\n"
+                        toSign="12345678901234567890" # 20 bytes, SHA1 digest
+                        print "* Signing with object 0x%08X following data: %s" % (o.value(), toSign)
+                        signature = session.sign(o, toSign) 
+                        s = ''.join(chr(c) for c in signature).encode('hex')
+                        sx = eval('0x%s' % s)
+                        print "Signature:"
+                        print dump(''.join(map(chr, signature)), 16)
+                        if m and e:
+                            print "Verifying using following public key:"
+                            print "Modulus:"
+                            print dump(''.join(map(chr, m)), 16)
+                            print "Exponent:"
+                            print dump(''.join(map(chr, e)), 16)
+                            decrypted = pow(sx, ex, mx) # RSA
+                            if toSign == hexx(decrypted).decode('hex')[-20:]:
+                                print "*** signature VERIFIED!\n"
+                            else:
+                                print "*** signature NOT VERIFIED; decrypted value:"
+                                print hex(decrypted), "\n"
                         else:
-                            print "decryption FAILED!\n"
+                            print "Unable to verify signature: MODULUS/PUBLIC_EXP not found"
                     except:
-                        print "Decrypt failed, exception:", str(sys.exc_info()[1])
+                        print "Sign failed, exception:", str(sys.exc_info()[1])
+                if decrypt:
+                    if m and e:
+                        try:
+                            toEncrypt="12345678901234567890"
+                            # note: PKCS1 BT2 padding should be random data,
+                            # but this is just a test and we use 0xFF...
+                            padded = "\x02%s\x00%s" % ("\xFF" * (128 - (len(toEncrypt)) -2), toEncrypt)
+                            print "* Decrypting with 0x%08X following data: %s" % (o.value(), toEncrypt)
+                            print "padded:\n",  dump(padded, 16)
+                            encrypted = pow(eval('0x%sL' % padded.encode('hex')), ex, mx) # RSA
+                            encrypted1 = hexx(encrypted).decode('hex')
+                            print "encrypted:\n", dump(encrypted1, 16)
+                            decrypted = session.decrypt(o, encrypted1)
+                            decrypted1 = ''.join(chr(i) for i in decrypted)
+                            print "decrypted:\n", dump(decrypted1, 16)
+                            if decrypted1 == toEncrypt:
+                                print "decryption SUCCESSFULL!\n"
+                            else:
+                                print "decryption FAILED!\n"
+                        except:
+                            print "Decrypt failed, exception:", str(sys.exc_info()[1])
+                    else:
+                        print "ERROR: Private key don't have MODULUS/PUBLIC_EXP"
+                    
+            print "Dumping attributes:"
+            for q, a in zip(all_attributes, attributes):
+                if a == None:
+                    # undefined (CKR_ATTRIBUTE_TYPE_INVALID) attribute
+                    continue
+                if q == PyKCS11.CKA_CLASS:
+                    print format_long % (PyKCS11.CKA[q], PyKCS11.CKO[a], a)
+                elif q == PyKCS11.CKA_CERTIFICATE_TYPE:
+                    print format_long % (PyKCS11.CKA[q], PyKCS11.CKC[a], a)
+                elif q == PyKCS11.CKA_KEY_TYPE:
+                    print format_long % (PyKCS11.CKA[q], PyKCS11.CKK[a], a)
+                elif session.isBin(q):
+                    print format_binary % (PyKCS11.CKA[q], len(a))
+                    if a:
+                        print dump(''.join(map(chr, a)), 16),
+                elif q == PyKCS11.CKA_SERIAL_NUMBER:
+                    print format_binary % (PyKCS11.CKA[q], len(a))
+                    if a:
+                        print dump(a, 16),
                 else:
-                    print "ERROR: Private key don't have MODULUS/PUBLIC_EXP"
-                
-        print "Dumping attributes:"
-        for q, a in zip(all_attributes, attributes):
-            if a == None:
-                # undefined (CKR_ATTRIBUTE_TYPE_INVALID) attribute
-                continue
-            if q == PyKCS11.CKA_CLASS:
-                print format_long % (PyKCS11.CKA[q], PyKCS11.CKO[a], a)
-            elif q == PyKCS11.CKA_CERTIFICATE_TYPE:
-                print format_long % (PyKCS11.CKA[q], PyKCS11.CKC[a], a)
-            elif q == PyKCS11.CKA_KEY_TYPE:
-                print format_long % (PyKCS11.CKA[q], PyKCS11.CKK[a], a)
-            elif session.isBin(q):
-                print format_binary % (PyKCS11.CKA[q], len(a))
-                if a:
-                    print dump(''.join(map(chr, a)), 16),
-            elif q == PyKCS11.CKA_SERIAL_NUMBER:
-                print format_binary % (PyKCS11.CKA[q], len(a))
-                if a:
-                    print dump(a, 16),
-            else:
-                print format_normal % (PyKCS11.CKA[q], a)
-    print
+                    print format_normal % (PyKCS11.CKA[q], a)
+        print
 
-    if pin_available:
-        try:
-            session.logout()
-        except:
-            print "logout failed, exception:", str(sys.exc_info()[1])
+        if pin_available:
+            try:
+                session.logout()
+            except:
+                print "logout failed, exception:", str(sys.exc_info()[1])
 
-    session.closeSession()
+        session.closeSession()
+
+    except PyKCS11.PyKCS11Error, e:
+        print "Error:", e
 
