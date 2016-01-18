839b58f7170343a7788bc7ae9db3dbb7cd690417
Ludovic Rousseau <LudovicRousseau@users.noreply.github.com>
2016-01-18 10:46:06 +0100

    Initial unittest test script

---
+++ tests/CK_test.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+# execute using:
+# python tests/CK_test.py
+
+import unittest
+import PyKCS11
+
+class TestUtil(unittest.TestCase):
+
+    def test_CKM(self):
+        self.assertEqual(PyKCS11.CKM_RSA_PKCS_KEY_PAIR_GEN, 0x00000000)
+        self.assertEqual(PyKCS11.CKM[PyKCS11.CKM_RSA_PKCS_KEY_PAIR_GEN],
+                         'CKM_RSA_PKCS_KEY_PAIR_GEN')
+
+        self.assertEqual(PyKCS11.CKM_VENDOR_DEFINED, 0x80000000L)
+
+    def test_CKR(self):
+        self.assertEqual(PyKCS11.CKR_VENDOR_DEFINED, 0x80000000L)
+
+if __name__ == '__main__':
+    unittest.main()
