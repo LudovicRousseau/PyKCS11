f25e7249aa6a90bf7a178f5b6d39a49f86c2ff42
Oleg Broytman <phd@phdru.name>
2016-09-21 18:42:51 +0300

    Fix sample: ckattrlist is in PyKCS11.LowLevel

--- samples/LowLevel/test.py
+++ samples/LowLevel/test.py
@@ -58,7 +58,7 @@ print("C_OpenSession(): " + hex(a.C_OpenSession(slotList[0], PyKCS11.LowLevel.CK
 print("C_Login(): " + hex(a.C_Login(session, PyKCS11.LowLevel.CKU_USER, pin)))
 
 SearchResult = PyKCS11.LowLevel.ckobjlist(10)
-SearchTemplate = PyKCS11.ckattrlist(2)
+SearchTemplate = PyKCS11.LowLevel.ckattrlist(2)
 SearchTemplate[0].SetNum(PyKCS11.LowLevel.CKA_CLASS, PyKCS11.LowLevel.CKO_CERTIFICATE)
 SearchTemplate[1].SetBool(PyKCS11.LowLevel.CKA_TOKEN, True)
 
@@ -68,7 +68,7 @@ print("C_FindObjectsFinal: " + hex(a.C_FindObjectsFinal(session)))
 
 for x in SearchResult:
     print("object: " + hex(x.value()))
-    valTemplate = PyKCS11.ckattrlist(2)
+    valTemplate = PyKCS11.LowLevel.ckattrlist(2)
     valTemplate[0].SetType(PyKCS11.LowLevel.CKA_LABEL)
     # valTemplate[0].Reserve(128)
     valTemplate[1].SetType(PyKCS11.LowLevel.CKA_CLASS)
