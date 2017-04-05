83e9131a11cb8b4fb0e196e89a8bebf5bfdefe81
Francesco Franchina <cescus92@gmail.com>
2017-04-05 16:26:02 +0200

    Fixed the test to run even under python3

--- tests/CK_test.py
+++ tests/CK_test.py
@@ -14,10 +14,10 @@ class TestUtil(unittest.TestCase):
         self.assertEqual(PyKCS11.CKM[PyKCS11.CKM_RSA_PKCS_KEY_PAIR_GEN],
                          'CKM_RSA_PKCS_KEY_PAIR_GEN')
 
-        self.assertEqual(PyKCS11.CKM_VENDOR_DEFINED, 0x80000000L)
+        self.assertEqual(PyKCS11.CKM_VENDOR_DEFINED, 0x80000000)
 
     def test_CKR(self):
-        self.assertEqual(PyKCS11.CKR_VENDOR_DEFINED, 0x80000000L)
+        self.assertEqual(PyKCS11.CKR_VENDOR_DEFINED, 0x80000000)
 
 if __name__ == '__main__':
     unittest.main()
