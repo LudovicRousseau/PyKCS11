fadd6511463eff54ae77178e430dadb83efe082a
Ludovic Rousseau <ludovic.rousseau@free.fr>
2017-05-31 18:29:15 +0200

    tests: add tests for class ckbytelist

---
+++ tests/test_ckbytelist.py
@@ -0,0 +1,32 @@
+import unittest
+import PyKCS11
+
+
+class Testutil(unittest.TestCase):
+
+    def test_empty(self):
+        ck = PyKCS11.ckbytelist()
+        self.assertSequenceEqual(ck, [])
+
+    def test_data(self):
+        size = 5
+        ck = PyKCS11.ckbytelist(size)
+        for index in range(size):
+            ck[index] = index
+        self.assertSequenceEqual(ck, range(size))
+
+    def test_length(self):
+        size = 5
+        ck = PyKCS11.ckbytelist(size)
+        self.assertEqual(len(ck), size)
+
+    def test_string(self):
+        size = 5
+        ck = PyKCS11.ckbytelist(size)
+        for index in range(size):
+            ck[index] = index
+        self.assertEqual(str(ck), str(range(size)))
+
+
+if __name__ == '__main__':
+    unittest.main()
