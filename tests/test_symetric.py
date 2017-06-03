92ac4e891c081f9d8c714666791f563f24c84e04
Ludovic Rousseau <ludovic.rousseau@free.fr>
2017-06-03 19:04:53 +0200

    test_asymetric: test symetric crypto

---
+++ tests/test_symetric.py
@@ -0,0 +1,56 @@
+import unittest
+from PyKCS11 import PyKCS11
+
+
+class TestUtil(unittest.TestCase):
+
+    def setUp(self):
+        self.pkcs11 = PyKCS11.PyKCS11Lib()
+        self.pkcs11.load()
+        self.slot = self.pkcs11.getSlotList(tokenPresent=True)[0]
+        self.session = self.pkcs11.openSession(self.slot,
+                                               PyKCS11.CKF_SERIAL_SESSION |
+                                               PyKCS11.CKF_RW_SESSION)
+        self.session.login("1234")
+
+    def tearDown(self):
+        self.session.logout()
+        self.pkcs11.closeAllSessions(self.slot)
+        del self.pkcs11
+
+    def test_sign(self):
+        keyID = (0x01,)
+        AESKeyTemplate = [
+                (PyKCS11.CKA_CLASS, PyKCS11.CKO_SECRET_KEY),
+                (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_AES),
+                (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+                (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
+                (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_TRUE),
+                (PyKCS11.CKA_DECRYPT, PyKCS11.CK_TRUE),
+                (PyKCS11.CKA_SIGN, PyKCS11.CK_FALSE),
+                (PyKCS11.CKA_VERIFY, PyKCS11.CK_FALSE),
+                (PyKCS11.CKA_VALUE_LEN, 32),
+                (PyKCS11.CKA_LABEL, "TestAESKey"),
+                (PyKCS11.CKA_ID, keyID)
+                ]
+
+        AESKey = self.session.generateKey(AESKeyTemplate)
+        self.assertIsNotNone(AESKey)
+
+        # buffer of 32 bytes 0x00
+        DataIn = [0] * 32
+
+        # AES CBC with IV
+        mechanism = PyKCS11.Mechanism(PyKCS11.CKM_AES_CBC, '1234567812345678')
+
+        # find the first secret key
+        symKey = self.session.findObjects([(PyKCS11.CKA_CLASS,
+            PyKCS11.CKO_SECRET_KEY), (PyKCS11.CKA_ID, keyID)])[0]
+
+        DataOut = self.session.encrypt(symKey, DataIn, mechanism)
+
+        DataCheck = self.session.decrypt(symKey, DataOut, mechanism)
+
+        self.assertSequenceEqual(DataIn, DataCheck)
+
+        self.session.destroyObject(AESKey)
