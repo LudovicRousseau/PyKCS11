23695646189a542bd52d8b0d525fc51605f09209
Ludovic Rousseau <ludovic.rousseau@free.fr>
2017-06-05 20:22:17 +0200

    test_info: test get*Info methods
    getInfo
    getSlotInfo
    getTokenInfo
    getSessionInfo

---
+++ tests/test_info.py
@@ -0,0 +1,32 @@
+import unittest
+from PyKCS11 import PyKCS11
+
+
+class TestUtil(unittest.TestCase):
+
+    def setUp(self):
+        self.pkcs11 = PyKCS11.PyKCS11Lib()
+        self.pkcs11.load()
+        self.slot = self.pkcs11.getSlotList(tokenPresent=True)[0]
+
+    def test_getInfo(self):
+        info = self.pkcs11.getInfo()
+        text = str(info)
+        self.assertIsNotNone(text)
+
+    def test_getSlotInfo(self):
+        info = self.pkcs11.getSlotInfo(self.slot)
+        text = str(info)
+        self.assertIsNotNone(text)
+
+    def test_getTokenInfo(self):
+        info = self.pkcs11.getTokenInfo(self.slot)
+        text = str(info)
+        self.assertIsNotNone(text)
+
+    def test_getSessionInfo(self):
+        self.session = self.pkcs11.openSession(self.slot,
+                                               PyKCS11.CKF_SERIAL_SESSION)
+        info = self.session.getSessionInfo()
+        text = str(info)
+        self.assertIsNotNone(text)
