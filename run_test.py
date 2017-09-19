06b63ff7fe83fa5801acf01b4c3e11b8ab3dcf55
Ludovic Rousseau <ludovic.rousseau@free.fr>
2017-09-19 23:36:01 +0200

    run_test: add support of OpenSC PKCS#11 spy
    Define PKCS11SPY to the PKCS#11 lib you want to use and the wrapper will
    try to find pkcs11-spy.so.

--- run_test.py
+++ run_test.py
@@ -14,10 +14,17 @@ pattern = "test*.py"
 if len(sys.argv) > 1:
     pattern = sys.argv[1]
 
-LIBS = ["/usr/local/lib/softhsm/libsofthsm2.so",  # macOS or local build
-        "/usr/lib/softhsm/libsofthsm2.so",  # Debian libsofthsm2
-        "/usr/lib/softhsm/libsofthsm.so",  # Debian libsofthsm
-        "/usr/lib/libsofthsm.so"]  # Ubuntu 12.04 libsofthsm
+if "PKCS11SPY" in os.environ:
+    # use OpenSC PKCS#11 spy if PKCS11SPY is defined
+    LIBS = ["/usr/local/lib/pkcs11/pkcs11-spy.so",  # macOS or local build
+            "/usr/lib/x86_64-linux-gnu/pkcs11-spy.so"]  # Debian amd64
+else:
+    # use SoftHSM2 or SoftHSM1
+    LIBS = ["/usr/local/lib/softhsm/libsofthsm2.so",  # macOS or local build
+            "/usr/lib/softhsm/libsofthsm2.so",  # Debian libsofthsm2
+            "/usr/lib/softhsm/libsofthsm.so",  # Debian libsofthsm
+            "/usr/lib/libsofthsm.so"]  # Ubuntu 12.04 libsofthsm
+
 for lib in LIBS:
     if os.path.isfile(lib):
         print("Using lib:", lib)
