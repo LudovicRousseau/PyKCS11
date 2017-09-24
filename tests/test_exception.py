bac308ebe09b8c8aafd2978e8480f155cfdc4cfb
Ludovic Rousseau <ludovic.rousseau@free.fr>
2017-09-24 18:38:14 +0200

    test_exception: new tests for CKR_VENDOR_DEFINED

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -26,3 +26,17 @@ class Testutil(unittest.TestCase):
     def test_raise(self):
         with self.assertRaises(PyKCS11.PyKCS11Error):
             raise PyKCS11.PyKCS11Error(0)
+
+    def test_unknown(self):
+        e = PyKCS11.PyKCS11Error(PyKCS11.CKR_VENDOR_DEFINED - 1)
+        self.assertEqual(str(e), "Unknown error (0x7FFFFFFF)")
+
+    def test_vendor0(self):
+        e = PyKCS11.PyKCS11Error(PyKCS11.CKR_VENDOR_DEFINED, "Pouet")
+        self.assertEqual(e.value, PyKCS11.CKR_VENDOR_DEFINED)
+        self.assertEqual(str(e), "Vendor error (0x00000000)")
+
+    def test_vendor10(self):
+        e = PyKCS11.PyKCS11Error(PyKCS11.CKR_VENDOR_DEFINED + 10)
+        self.assertEqual(e.value, PyKCS11.CKR_VENDOR_DEFINED + 10)
+        self.assertEqual(str(e), "Vendor error (0x0000000A)")
