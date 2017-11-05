0ce351d4168936cb5bc04b8b854a9e5340e8d7ea
Ludovic Rousseau <ludovic.rousseau@free.fr>
2017-11-05 22:22:35 +0100

    Fix support of RSA PKCS PSS mechanism
    The mechanism object now uses a new parameter instead of hard coding the
    value to CKM_RSA_PKCS_PSS.

--- PyKCS11/__init__.py
+++ PyKCS11/__init__.py
@@ -767,20 +767,21 @@ class RSAOAEPMechanism(object):
 class RSA_PSS_Mechanism(object):
     """RSA PSS Wrapping mechanism"""
 
-    def __init__(self, hashAlg, mgf, sLen):
+    def __init__(self, mecha, hashAlg, mgf, sLen):
         """
+        @param mecha: the mechanism to use (like CKM_SHA384_RSA_PKCS_PSS)
         @param hashAlg: the hash algorithm to use (like CKM_SHA384)
         @param mgf: the mask generation function to use (like
         CKG_MGF1_SHA384)
         @param sLen: length, in bytes, of the salt value used in the PSS
-        encoding
+        encoding (like 0 or the message length)
         """
         self._param = PyKCS11.LowLevel.CK_RSA_PKCS_PSS_PARAMS()
         self._param.hashAlg = hashAlg
         self._param.mgf = mgf
         self._param.sLen = sLen
         self._mech = PyKCS11.LowLevel.CK_MECHANISM()
-        self._mech.mechanism = CKM_RSA_PKCS_PSS
+        self._mech.mechanism = mecha
         self._mech.pParameter = self._param
         self._mech.ulParameterLen = PyKCS11.LowLevel.CK_RSA_PKCS_PSS_PARAMS_LENGTH
 
