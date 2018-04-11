97b3953f7b73df7506d8f93b7569778487170d00
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-04-11 15:27:27 +0200

    reset_softHSM: also erase SoftHSM tokens on macOS
    By default they are stored in /usr/local/var/lib/softhsm/tokens/*

--- tests/reset_softHSM.sh
+++ tests/reset_softHSM.sh
@@ -5,4 +5,6 @@ set -e
 # (re)create a PKCS#11 token using SoftHSM v2
 
 rm -rf /var/lib/softhsm/tokens/*
+rm -rf /usr/local/var/lib/softhsm/tokens/*
+
 softhsm2-util --init-token --label "A token" --pin 1234 --so-pin 123456 --slot 0
