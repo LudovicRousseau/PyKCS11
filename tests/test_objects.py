666a2c0e5e294bf85b3cf6eca44db0179d9ae773
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-06-22 23:01:24 +0200

    test_objects: use .skipTest() for SoftHSM 1
    Regroup the tests to have as simpler code.

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -7,12 +7,25 @@ class TestUtil(unittest.TestCase):
     def setUp(self):
         self.pkcs11 = PyKCS11.PyKCS11Lib()
         self.pkcs11.load()
+
+        # get SoftHSM major version
+        self.SoftHSMversion = self.pkcs11.getInfo().libraryVersion[0]
+
         self.slot = self.pkcs11.getSlotList(tokenPresent=True)[0]
 
         self.session = self.pkcs11.openSession(self.slot,
                                                PyKCS11.CKF_SERIAL_SESSION |
                                                PyKCS11.CKF_RW_SESSION)
         self.session.login("1234")
+
+    def tearDown(self):
+        self.session.logout()
+        self.pkcs11.closeAllSessions(self.slot)
+        del self.pkcs11
+
+    def test_Objects(self):
+        if (self.SoftHSMversion < 2):
+            self.skipTest("generateKey() only supported by SoftHSM >= 2")
 
         AESKeyTemplate = [
                 (PyKCS11.CKA_CLASS, PyKCS11.CKO_SECRET_KEY),
@@ -28,61 +41,38 @@ class TestUtil(unittest.TestCase):
                 (PyKCS11.CKA_ID, (0x01,))
                 ]
 
-        self.AESKey = None
-        try:
-            AESKey = self.session.generateKey(AESKeyTemplate)
-        except PyKCS11.PyKCS11Error as e:
-            # generateKey() is not support by SoftHSM1
-            if e.value == PyKCS11.CKR_FUNCTION_NOT_SUPPORTED:
-                return
-            else:
-                raise
+        # generate AES key
+        AESKey = self.session.generateKey(AESKeyTemplate)
         self.assertIsNotNone(AESKey)
-        self.AESKey = AESKey
-
-    def tearDown(self):
-        if self.AESKey is not None:
-            self.session.destroyObject(self.AESKey)
-
-        self.session.logout()
-        self.pkcs11.closeAllSessions(self.slot)
-        del self.pkcs11
-
-    def test_Objecthandle(self):
-        if self.AESKey is None:
-            return
 
         # find the first secret key
         symKey = self.session.findObjects([(PyKCS11.CKA_CLASS,
                                             PyKCS11.CKO_SECRET_KEY)])[0]
 
+        # test object handle
         text = str(symKey)
         self.assertIsNotNone(text)
 
-    def test_CreateObject(self):
+        # test createObject()
         template = [
                 (PyKCS11.CKA_CLASS, PyKCS11.CKO_DATA),
                 (PyKCS11.CKA_LABEL, "data"),
                 ]
-        try:
-            handle = self.session.createObject(template)
-        except PyKCS11.PyKCS11Error as e:
-            # createObject() is not fully support by SoftHSM1
-            if e.value == PyKCS11.CKR_ATTRIBUTE_VALUE_INVALID:
-                return
+        handle = self.session.createObject(template)
+        self.assertIsNotNone(handle)
 
         self.session.destroyObject(handle)
 
-    def test_getAttributeValue(self):
-        if self.AESKey is None:
-            return
+        # test getAttributeValue
 
         # attributes as define by AESKeyTemplate
         all_attributes = [PyKCS11.CKA_CLASS, PyKCS11.CKA_KEY_TYPE,
                           PyKCS11.CKA_TOKEN, PyKCS11.CKA_LABEL,
                           PyKCS11.CKA_ID]
 
-        values = self.session.getAttributeValue(self.AESKey, all_attributes)
+        values = self.session.getAttributeValue(AESKey, all_attributes)
         self.assertEqual(values, [PyKCS11.CKO_SECRET_KEY,
                                   PyKCS11.CKK_AES, PyKCS11.CK_TRUE,
                                   "TestAESKey", (0x01,)])
+
+        self.session.destroyObject(AESKey)
