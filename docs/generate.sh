3d267070148c88caaa61a413f72c4d30e6813b10
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-06-25 23:03:31 +0200

    Sphinx: check the script is run from a virtualenv
    No need to install the package in the system folder. A virtualenv is
    also OK.

--- docs/generate.sh
+++ docs/generate.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Enable a virtualenv before running the script to avoid modifying
-# the system Python libraries
+# Enable a virtualenv before running the script to avoid modifying
+# the system Python libraries
+
+if [ ! "$VIRTUAL_ENV" ]
+then
+	echo "Run it from inside a virtualenv"
+	exit 1
+fi
 
 cd $(dirname $0)
 
