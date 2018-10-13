500e43a09c409c7b9f472b6ba643776c736fd190
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-10-13 16:49:19 +0200

    setup.py: fix pycodestyle errors
    setup.py:20:1: E128 continuation line under-indented for visual indent
    setup.py:21:1: E128 continuation line under-indented for visual indent
    setup.py:22:1: E128 continuation line under-indented for visual indent
    setup.py:23:1: E128 continuation line under-indented for visual indent
    setup.py:24:1: E128 continuation line under-indented for visual indent
    setup.py:25:1: E128 continuation line under-indented for visual indent
    setup.py:26:1: E128 continuation line under-indented for visual indent
    setup.py:27:1: E128 continuation line under-indented for visual indent
    setup.py:28:1: E128 continuation line under-indented for visual indent
    setup.py:29:1: E128 continuation line under-indented for visual indent
    setup.py:30:1: E128 continuation line under-indented for visual indent
    setup.py:52:80: E501 line too long (100 > 79 characters)
    setup.py:57:1: E302 expected 2 blank lines, found 1
    setup.py:64:1: E305 expected 2 blank lines after class or function definition, found 1
    setup.py:65:5: E128 continuation line under-indented for visual indent
    setup.py:66:5: E128 continuation line under-indented for visual indent
    setup.py:67:5: E128 continuation line under-indented for visual indent
    setup.py:68:5: E128 continuation line under-indented for visual indent
    setup.py:69:5: E128 continuation line under-indented for visual indent
    setup.py:70:5: E128 continuation line under-indented for visual indent
    setup.py:71:5: E128 continuation line under-indented for visual indent
    setup.py:72:5: E128 continuation line under-indented for visual indent
    setup.py:73:5: E128 continuation line under-indented for visual indent
    setup.py:74:5: E128 continuation line under-indented for visual indent
    setup.py:75:5: E128 continuation line under-indented for visual indent
    setup.py:76:5: E128 continuation line under-indented for visual indent
    setup.py:77:5: E128 continuation line under-indented for visual indent
    setup.py:78:5: E128 continuation line under-indented for visual indent
    setup.py:79:5: E128 continuation line under-indented for visual indent
    setup.py:90:5: E128 continuation line under-indented for visual indent
    setup.py:91:1: E124 closing bracket does not match visual indentation

--- setup.py
+++ setup.py
@@ -17,17 +17,17 @@ hardware crypto tokens, and almost all PKCS#11 functions and data types.
 The wrapper has been generated with the help of the SWIG compiler.'''
 
 classifiers = ["Development Status :: 5 - Production/Stable",
-"Intended Audience :: Developers",
-"License :: OSI Approved :: GNU General Public License (GPL)",
-"Natural Language :: English",
-"Operating System :: Microsoft :: Windows",
-"Operating System :: OS Independent",
-"Operating System :: Unix",
-"Programming Language :: C",
-"Programming Language :: C++",
-"Programming Language :: Python",
-"Topic :: Security :: Cryptography",
-"Topic :: Software Development :: Libraries :: Python Modules"]
+               "Intended Audience :: Developers",
+               "License :: OSI Approved :: GNU General Public License (GPL)",
+               "Natural Language :: English",
+               "Operating System :: Microsoft :: Windows",
+               "Operating System :: OS Independent",
+               "Operating System :: Unix",
+               "Programming Language :: C",
+               "Programming Language :: C++",
+               "Programming Language :: Python",
+               "Topic :: Security :: Cryptography",
+               "Topic :: Software Development :: Libraries :: Python Modules"]
 
 lib_dirs = []
 inc_dirs = ["src"]
@@ -49,10 +49,12 @@ if (platform.system().lower() == 'windows'):
     source_files.append("pykcs11.rc")
     libraries_val = ["python%d%d" % pyver[:2]]
     extra_compile_args = ["/Fdvc70.pdb", "/Zi", "/GR", "/EHsc"]
-    extra_link_args = ["/DEBUG", "/PDB:_LowLevel.pdb", "/SUBSYSTEM:WINDOWS", "/OPT:REF", "/OPT:ICF"]
+    extra_link_args = ["/DEBUG", "/PDB:_LowLevel.pdb", "/SUBSYSTEM:WINDOWS",
+                       "/OPT:REF", "/OPT:ICF"]
 else:
     source_files.append("src/dyn_unix.c")
     libraries_val = []
+
 
 class my_build(build):
     def run(self):
@@ -61,22 +63,23 @@ class my_build(build):
         self.run_command("build_py")
         build.run(self)
 
+
 setup(name="PyKCS11",
-    version="1.5.3",
-    description="A Full PKCS#11 wrapper for Python",
-    keywords="crypto,pki,pkcs11,c++",
-    classifiers=classifiers,
-    platforms="Win32 Unix",
-    long_description=description,
-    author="Giuseppe Amato (Midori)",
-    author_email="paipai at tiscali.it",
-    maintainer="Ludovic Rousseau",
-    maintainer_email="ludovic.rousseau@free.fr",
-    url="https://github.com/LudovicRousseau/PyKCS11",
-    download_url="http://sourceforge.net/projects/pkcs11wrap/files/pykcs11/",
-    license="GPL",
-    cmdclass={'build': my_build},
-    ext_modules=[
+      version="1.5.3",
+      description="A Full PKCS#11 wrapper for Python",
+      keywords="crypto,pki,pkcs11,c++",
+      classifiers=classifiers,
+      platforms="Win32 Unix",
+      long_description=description,
+      author="Giuseppe Amato (Midori)",
+      author_email="paipai at tiscali.it",
+      maintainer="Ludovic Rousseau",
+      maintainer_email="ludovic.rousseau@free.fr",
+      url="https://github.com/LudovicRousseau/PyKCS11",
+      download_url="http://sourceforge.net/projects/pkcs11wrap/files/pykcs11/",
+      license="GPL",
+      cmdclass={'build': my_build},
+      ext_modules=[
         Extension(
             "PyKCS11._LowLevel",
             sources=source_files,
@@ -87,5 +90,5 @@ setup(name="PyKCS11",
             swig_opts=['-c++'],
             extra_compile_args=extra_compile_args,
             extra_link_args=extra_link_args)],
-    py_modules=["PyKCS11.__init__", "PyKCS11.LowLevel"],
-)
+      py_modules=["PyKCS11.__init__", "PyKCS11.LowLevel"],
+      )
