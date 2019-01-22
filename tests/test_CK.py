a8c221031e846372c11c709cb9358d240b3ba6e5
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-01-22 21:51:08 +0100

    tests/test_CK.py: code reformat using black(1)

--- tests/test_CK.py
+++ tests/test_CK.py
@@ -8,11 +8,11 @@ import PyKCS11
 
 
 class TestUtil(unittest.TestCase):
-
     def test_CKM(self):
         self.assertEqual(PyKCS11.CKM_RSA_PKCS_KEY_PAIR_GEN, 0x00000000)
-        self.assertEqual(PyKCS11.CKM[PyKCS11.CKM_RSA_PKCS_KEY_PAIR_GEN],
-                         'CKM_RSA_PKCS_KEY_PAIR_GEN')
+        self.assertEqual(
+            PyKCS11.CKM[PyKCS11.CKM_RSA_PKCS_KEY_PAIR_GEN], "CKM_RSA_PKCS_KEY_PAIR_GEN"
+        )
 
         self.assertEqual(PyKCS11.CKM_VENDOR_DEFINED, 0x80000000)
 
@@ -20,5 +20,5 @@ class TestUtil(unittest.TestCase):
         self.assertEqual(PyKCS11.CKR_VENDOR_DEFINED, 0x80000000)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
