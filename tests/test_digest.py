afd61931c410a5028ccdb9d40bfb183d6878759e
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-01-22 21:51:09 +0100

    tests/test_digest.py: code reformat using black(1)

--- tests/test_digest.py
+++ tests/test_digest.py
@@ -7,18 +7,36 @@ import unittest
 from PyKCS11 import PyKCS11
 
 # SHA1 of "abc"
-SHA1_abc = [0xa9, 0x99, 0x3e, 0x36, 0x47, 0x6, 0x81, 0x6a, 0xba, 0x3e,
-            0x25, 0x71, 0x78, 0x50, 0xc2, 0x6c, 0x9c, 0xd0, 0xd8, 0x9d]
+SHA1_abc = [
+    0xA9,
+    0x99,
+    0x3E,
+    0x36,
+    0x47,
+    0x6,
+    0x81,
+    0x6A,
+    0xBA,
+    0x3E,
+    0x25,
+    0x71,
+    0x78,
+    0x50,
+    0xC2,
+    0x6C,
+    0x9C,
+    0xD0,
+    0xD8,
+    0x9D,
+]
 
 
 class TestUtil(unittest.TestCase):
-
     def setUp(self):
         self.pkcs11 = PyKCS11.PyKCS11Lib()
         self.pkcs11.load()
         self.slot = self.pkcs11.getSlotList(tokenPresent=True)[0]
-        self.session = self.pkcs11.openSession(self.slot,
-                                               PyKCS11.CKF_SERIAL_SESSION)
+        self.session = self.pkcs11.openSession(self.slot, PyKCS11.CKF_SERIAL_SESSION)
 
     def tearDown(self):
         self.pkcs11.closeAllSessions(self.slot)
@@ -35,5 +53,5 @@ class TestUtil(unittest.TestCase):
         self.assertSequenceEqual(digest, SHA1_abc)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
