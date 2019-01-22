93350a3965c6b96cb407d04712e137b4eed57392
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-01-22 21:51:09 +0100

    tests/test_asymetric_gost.py: code reformat using black(1)

--- tests/test_asymetric_gost.py
+++ tests/test_asymetric_gost.py
@@ -3,7 +3,6 @@ from PyKCS11 import PyKCS11
 
 
 class TestUtil(unittest.TestCase):
-
     def setUp(self):
         self.pkcs11 = PyKCS11.PyKCS11Lib()
         self.pkcs11.load()
@@ -12,9 +11,9 @@ class TestUtil(unittest.TestCase):
         self.SoftHSMversion = self.pkcs11.getInfo().libraryVersion[0]
 
         self.slot = self.pkcs11.getSlotList(tokenPresent=True)[0]
-        self.session = self.pkcs11.openSession(self.slot,
-                                               PyKCS11.CKF_SERIAL_SESSION |
-                                               PyKCS11.CKF_RW_SESSION)
+        self.session = self.pkcs11.openSession(
+            self.slot, PyKCS11.CKF_SERIAL_SESSION | PyKCS11.CKF_RW_SESSION
+        )
         self.session.login("1234")
 
     def tearDown(self):
@@ -23,12 +22,12 @@ class TestUtil(unittest.TestCase):
         del self.pkcs11
 
     def test_gost(self):
-        if (self.SoftHSMversion < 2):
+        if self.SoftHSMversion < 2:
             self.skipTest("generateKeyPair() only supported by SoftHSM >= 2")
 
         # values from SoftHSMv2
-        param_a = (0x06, 0x07, 0x2a, 0x85, 0x03, 0x02, 0x02, 0x23, 0x01)
-        param_b = (0x06, 0x07, 0x2a, 0x85, 0x03, 0x02, 0x02, 0x1e, 0x01)
+        param_a = (0x06, 0x07, 0x2A, 0x85, 0x03, 0x02, 0x02, 0x23, 0x01)
+        param_b = (0x06, 0x07, 0x2A, 0x85, 0x03, 0x02, 0x02, 0x1E, 0x01)
 
         keyID = (0x23,)
         pubTemplate = [
@@ -49,15 +48,15 @@ class TestUtil(unittest.TestCase):
             (PyKCS11.CKA_DECRYPT, PyKCS11.CK_TRUE),
             (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
             (PyKCS11.CKA_UNWRAP, PyKCS11.CK_TRUE),
-            (PyKCS11.CKA_ID, keyID)
+            (PyKCS11.CKA_ID, keyID),
         ]
 
         # test generate gost key pair
-        gen_mechanism = PyKCS11.Mechanism(PyKCS11.CKM_GOSTR3410_KEY_PAIR_GEN,
-                                          None)
+        gen_mechanism = PyKCS11.Mechanism(PyKCS11.CKM_GOSTR3410_KEY_PAIR_GEN, None)
         try:
-            (self.pubKey, self.privKey) = self.session.generateKeyPair(pubTemplate,
-                    privTemplate, gen_mechanism)
+            (self.pubKey, self.privKey) = self.session.generateKeyPair(
+                pubTemplate, privTemplate, gen_mechanism
+            )
         except PyKCS11.PyKCS11Error as e:
             if e.value == PyKCS11.CKR_MECHANISM_INVALID:
                 self.skipTest("GOST not supported by SoftHSMv2 on Windows?")
@@ -74,8 +73,7 @@ class TestUtil(unittest.TestCase):
         # sign/verify
         signature = self.session.sign(self.privKey, toSign, mecha)
 
-        result = self.session.verify(self.pubKey, toSign, signature,
-                                     mecha)
+        result = self.session.verify(self.pubKey, toSign, signature, mecha)
         self.assertTrue(result)
 
         # test CK_OBJECT_HANDLE.__repr__()
