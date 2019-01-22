384b1c39231ad05844da27c42e1f1c9bc5c29d18
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-01-22 21:51:08 +0100

    setup.py: code reformat using black(1)

--- setup.py
+++ setup.py
@@ -10,24 +10,26 @@ from sys import version_info as pyver
 from os import path, system
 import platform
 
-description = '''A complete PKCS#11 wrapper for Python.
+description = """A complete PKCS#11 wrapper for Python.
 You can use any PKCS#11 (aka CryptoKi) module such as the PSM which
 comes as part of mozilla or the various modules supplied by vendors of
 hardware crypto tokens, and almost all PKCS#11 functions and data types.
-The wrapper has been generated with the help of the SWIG compiler.'''
+The wrapper has been generated with the help of the SWIG compiler."""
 
-classifiers = ["Development Status :: 5 - Production/Stable",
-               "Intended Audience :: Developers",
-               "License :: OSI Approved :: GNU General Public License (GPL)",
-               "Natural Language :: English",
-               "Operating System :: Microsoft :: Windows",
-               "Operating System :: OS Independent",
-               "Operating System :: Unix",
-               "Programming Language :: C",
-               "Programming Language :: C++",
-               "Programming Language :: Python",
-               "Topic :: Security :: Cryptography",
-               "Topic :: Software Development :: Libraries :: Python Modules"]
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Natural Language :: English",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: OS Independent",
+    "Operating System :: Unix",
+    "Programming Language :: C",
+    "Programming Language :: C++",
+    "Programming Language :: Python",
+    "Topic :: Security :: Cryptography",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
 
 lib_dirs = []
 inc_dirs = ["src"]
@@ -35,22 +37,29 @@ inc_dirs = ["src"]
 if path.exists("/usr/local"):
     lib_dirs.append("/usr/local/lib")
     inc_dirs.append("/usr/local/include")
-source_files = ["src/ck_attribute_smart.cpp",
-                "src/pkcs11lib.cpp",
-                "src/pykcs11string.cpp",
-                "src/utility.cpp",
-                "src/pykcs11.i",
-                "src/pykcs11.cpp"]
+source_files = [
+    "src/ck_attribute_smart.cpp",
+    "src/pkcs11lib.cpp",
+    "src/pykcs11string.cpp",
+    "src/utility.cpp",
+    "src/pykcs11.i",
+    "src/pykcs11.cpp",
+]
 define_macros = []
 extra_compile_args = []
 extra_link_args = []
-if (platform.system().lower() == 'windows'):
+if platform.system().lower() == "windows":
     source_files.append("src/dyn_win32.c")
     source_files.append("pykcs11.rc")
     libraries_val = ["python%d%d" % pyver[:2]]
     extra_compile_args = ["/Fdvc70.pdb", "/Zi", "/GR", "/EHsc"]
-    extra_link_args = ["/DEBUG", "/PDB:_LowLevel.pdb", "/SUBSYSTEM:WINDOWS",
-                       "/OPT:REF", "/OPT:ICF"]
+    extra_link_args = [
+        "/DEBUG",
+        "/PDB:_LowLevel.pdb",
+        "/SUBSYSTEM:WINDOWS",
+        "/OPT:REF",
+        "/OPT:ICF",
+    ]
 else:
     source_files.append("src/dyn_unix.c")
     libraries_val = []
@@ -64,22 +73,23 @@ class my_build(build):
         build.run(self)
 
 
-setup(name="PyKCS11",
-      version="1.5.3",
-      description="A Full PKCS#11 wrapper for Python",
-      keywords="crypto,pki,pkcs11,c++",
-      classifiers=classifiers,
-      platforms="Win32 Unix",
-      long_description=description,
-      author="Giuseppe Amato (Midori)",
-      author_email="paipai at tiscali.it",
-      maintainer="Ludovic Rousseau",
-      maintainer_email="ludovic.rousseau@free.fr",
-      url="https://github.com/LudovicRousseau/PyKCS11",
-      download_url="http://sourceforge.net/projects/pkcs11wrap/files/pykcs11/",
-      license="GPL",
-      cmdclass={'build': my_build},
-      ext_modules=[
+setup(
+    name="PyKCS11",
+    version="1.5.3",
+    description="A Full PKCS#11 wrapper for Python",
+    keywords="crypto,pki,pkcs11,c++",
+    classifiers=classifiers,
+    platforms="Win32 Unix",
+    long_description=description,
+    author="Giuseppe Amato (Midori)",
+    author_email="paipai at tiscali.it",
+    maintainer="Ludovic Rousseau",
+    maintainer_email="ludovic.rousseau@free.fr",
+    url="https://github.com/LudovicRousseau/PyKCS11",
+    download_url="http://sourceforge.net/projects/pkcs11wrap/files/pykcs11/",
+    license="GPL",
+    cmdclass={"build": my_build},
+    ext_modules=[
         Extension(
             "PyKCS11._LowLevel",
             sources=source_files,
@@ -87,8 +97,10 @@ setup(name="PyKCS11",
             library_dirs=lib_dirs,
             libraries=libraries_val,
             define_macros=define_macros,
-            swig_opts=['-c++'],
+            swig_opts=["-c++"],
             extra_compile_args=extra_compile_args,
-            extra_link_args=extra_link_args)],
-      py_modules=["PyKCS11.__init__", "PyKCS11.LowLevel"],
-      )
+            extra_link_args=extra_link_args,
+        )
+    ],
+    py_modules=["PyKCS11.__init__", "PyKCS11.LowLevel"],
+)
