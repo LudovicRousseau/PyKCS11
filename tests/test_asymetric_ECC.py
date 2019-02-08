eb39e565378f449ff46c79501b41296f8a0ad396
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-02-08 14:38:41 +0100

    Add test_asymetric_ECC

---
+++ tests/test_asymetric_ECC.py
@@ -0,0 +1,95 @@
+import unittest
+from PyKCS11 import PyKCS11
+from asn1crypto.keys import ECDomainParameters, NamedCurve
+
+
+class TestUtil(unittest.TestCase):
+    def setUp(self):
+        self.pkcs11 = PyKCS11.PyKCS11Lib()
+        self.pkcs11.load()
+
+        # get SoftHSM major version
+        self.SoftHSMversion = self.pkcs11.getInfo().libraryVersion[0]
+
+        self.slot = self.pkcs11.getSlotList(tokenPresent=True)[0]
+        self.session = self.pkcs11.openSession(
+            self.slot, PyKCS11.CKF_SERIAL_SESSION | PyKCS11.CKF_RW_SESSION
+        )
+        self.session.login("1234")
+
+        # Select the curve to be used for the keys
+        curve = "secp256r1"
+
+        # Setup the domain parameters, unicode conversion needed
+        # for the curve string
+        domain_params = ECDomainParameters(name="named", value=NamedCurve.unmap(curve))
+        ec_params = domain_params.dump()
+
+        keyID = (0x22,)
+        label = 'test'
+
+        ec_public_tmpl = [
+            (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
+            (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
+            (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+            (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_TRUE),
+            (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
+            (PyKCS11.CKA_WRAP, PyKCS11.CK_TRUE),
+            (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+            (PyKCS11.CKA_EC_PARAMS, ec_params),
+            (PyKCS11.CKA_LABEL, label),
+            (PyKCS11.CKA_ID, keyID),
+        ]
+
+        ec_priv_tmpl = [
+            (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
+            (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+            (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+            (PyKCS11.CKA_SENSITIVE, PyKCS11.CK_TRUE),
+            (PyKCS11.CKA_DECRYPT, PyKCS11.CK_TRUE),
+            (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
+            (PyKCS11.CKA_UNWRAP, PyKCS11.CK_TRUE),
+            (PyKCS11.CKA_LABEL, label),
+            (PyKCS11.CKA_ID, keyID),
+        ]
+
+        (self.pubKey, self.privKey) = self.session.generateKeyPair(
+            ec_public_tmpl, ec_priv_tmpl, mecha=PyKCS11.MechanismECGENERATEKEYPAIR
+        )
+        self.assertIsNotNone(self.pubKey)
+        self.assertIsNotNone(self.privKey)
+
+        # test CK_OBJECT_HANDLE.__repr__()
+        text = str(self.pubKey)
+        self.assertIsNotNone(text)
+
+    def tearDown(self):
+        self.session.destroyObject(self.pubKey)
+        self.session.destroyObject(self.privKey)
+
+        self.session.logout()
+        self.pkcs11.closeAllSessions(self.slot)
+        del self.pkcs11
+
+    def test_sign_integer(self):
+        toSign = 1234567890
+        mecha = PyKCS11.Mechanism(PyKCS11.CKM_ECDSA, None)
+
+        # sign/verify
+        try:
+            self.session.sign(self.privKey, toSign, mecha)
+        except PyKCS11.PyKCS11Error as e:
+            # should return PyKCS11.PyKCS11Error: Unknown format (<class 'int'>)
+            self.assertEqual(e.value, -3)
+
+    def test_sign_text(self):
+        toSign = "Hello World!"
+        mecha = PyKCS11.Mechanism(PyKCS11.CKM_ECDSA, None)
+
+        # sign/verify
+        signature = self.session.sign(self.privKey, toSign, mecha)
+
+        result = self.session.verify(self.pubKey, toSign, signature, mecha)
+
+        self.assertTrue(result)
+
