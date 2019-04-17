3b4f3e39490c34ea551c524d42c908ef0f5b3325
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-04-17 15:43:17 +0200

    samples/ec_generate.py: code reformat using black(1)

--- samples/ec_generate.py
+++ samples/ec_generate.py
@@ -28,41 +28,43 @@ session = pkcs11.openSession(slot, CKF_SERIAL_SESSION | CKF_RW_SESSION)
 session.login("1234")
 
 key_id = (0x22,)
-label = 'test'
+label = "test"
 
 # Select the curve to be used for the keys
-curve = u'secp256r1'
+curve = u"secp256r1"
 
 # Setup the domain parameters, unicode conversion needed for the curve string
-domain_params = ECDomainParameters(name='named', value=NamedCurve(curve))
+domain_params = ECDomainParameters(name="named", value=NamedCurve(curve))
 ec_params = domain_params.dump()
 
 ec_public_tmpl = [
-        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
-        (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
-        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_WRAP, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
-        (PyKCS11.CKA_EC_PARAMS, ec_params),
-        (PyKCS11.CKA_LABEL, label),
-        (PyKCS11.CKA_ID, key_id)
-    ]
+    (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
+    (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
+    (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_WRAP, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+    (PyKCS11.CKA_EC_PARAMS, ec_params),
+    (PyKCS11.CKA_LABEL, label),
+    (PyKCS11.CKA_ID, key_id),
+]
 
 ec_priv_tmpl = [
-        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
-        (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
-        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_SENSITIVE, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_DECRYPT, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_UNWRAP, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_LABEL, label),
-        (PyKCS11.CKA_ID, key_id)
-    ]
+    (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
+    (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+    (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_SENSITIVE, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_DECRYPT, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_UNWRAP, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_LABEL, label),
+    (PyKCS11.CKA_ID, key_id),
+]
 
-(pub_key, priv_key) = session.generateKeyPair(ec_public_tmpl, ec_priv_tmpl, mecha=PyKCS11.MechanismECGENERATEKEYPAIR)
+(pub_key, priv_key) = session.generateKeyPair(
+    ec_public_tmpl, ec_priv_tmpl, mecha=PyKCS11.MechanismECGENERATEKEYPAIR
+)
 
 session.logout()
 session.closeSession()
