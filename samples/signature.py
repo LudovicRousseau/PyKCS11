39d6784cd710aa14bb3e5cc8b69c6e8304df58e0
Ludovic Rousseau <ludovic.rousseau@free.fr>
2023-04-03 17:21:04 +0200

    sample signature: sign a text message
    Do not use a string encoded in hex since that does not bring any value.

--- samples/signature.py
+++ samples/signature.py
@@ -17,8 +17,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA.
 
-from __future__ import print_function
-
 from PyKCS11 import *
 import binascii
 
@@ -31,18 +29,18 @@ slot = pkcs11.getSlotList(tokenPresent=True)[0]
 session = pkcs11.openSession(slot, CKF_SERIAL_SESSION | CKF_RW_SESSION)
 session.login("1234")
 
-# "Hello world" in hex
-toSign = "48656c6c6f20776f726c640d0a"
+# message to sign
+toSign = "Hello World!"
 mechanism = Mechanism(CKM_SHA256_RSA_PKCS, None)
 
 # find first private key and compute signature
 privKey = session.findObjects([(CKA_CLASS, CKO_PRIVATE_KEY)])[0]
-signature = session.sign(privKey, binascii.unhexlify(toSign), mechanism)
+signature = session.sign(privKey, toSign, mechanism)
 print("\nsignature: {}".format(binascii.hexlify(bytearray(signature))))
 
 # find first public key and verify signature
 pubKey = session.findObjects([(CKA_CLASS, CKO_PUBLIC_KEY)])[0]
-result = session.verify(pubKey, binascii.unhexlify(toSign), signature, mechanism)
+result = session.verify(pubKey, toSign, signature, mechanism)
 print("\nVerified:", result)
 
 # logout
