6c3e25035c6a05ff687de23ef35e26df0588b111
Ludovic Rousseau <ludovic.rousseau@free.fr>
2024-05-24 17:40:19 +0200

    Release 1.5.16
    Signed-off-by: Ludovic Rousseau <ludovic.rousseau@free.fr>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Giuseppe Amato (Midori), Ludovic Rousseau'
 # The short X.Y version
 version = '1.5'
 # The full version, including alpha/beta/rc tags
-release = '1.5.15'
+release = '1.5.16'
 
 
 # -- General configuration ---------------------------------------------------
