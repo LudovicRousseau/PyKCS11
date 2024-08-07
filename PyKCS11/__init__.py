0a080f429bc9309a64c508ef3cb3f1b420ca943b
fuzzykat <70575698+fuzzykat@users.noreply.github.com>
2024-08-07 14:48:53 +0300

    add AES in counter mode support

--- PyKCS11/__init__.py
+++ PyKCS11/__init__.py
@@ -812,6 +812,29 @@ class AES_GCM_Mechanism(object):
         return self._mech
 
 
+class AES_CTR_Mechanism(object):
+    """CKM_AES_CTR encryption mechanism"""
+
+    def __init__(self, counterBits, counterBlock):
+        """
+        :param counterBits: the number of incremented bits in the counter block
+        :param counterBlock: a 16-byte initial value of the counter block
+        """
+        self._param = PyKCS11.LowLevel.CK_AES_CTR_PARAMS()
+
+        self._source_cb = ckbytelist(counterBlock)
+        self._param.ulCounterBits = counterBits
+        self._param.cb = self._source_cb
+
+        self._mech = PyKCS11.LowLevel.CK_MECHANISM()
+        self._mech.mechanism = CKM_AES_CTR
+        self._mech.pParameter = self._param
+        self._mech.ulParameterLen = PyKCS11.LowLevel.CK_AES_CTR_PARAMS_LENGTH
+
+    def to_native(self):
+        return self._mech
+
+
 class RSAOAEPMechanism(object):
     """RSA OAEP Wrapping mechanism"""
 
