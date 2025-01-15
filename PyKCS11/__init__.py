7083cd10ca333f0298abc676581f5a0b6fa929af
fuzzykat <70575698+fuzzykat@users.noreply.github.com>
2025-01-15 15:38:19 +0300

    bugfix: store CKM_CONCATENATE_BASE_AND_KEY parameter in mechanism context
    It's needed to be sure that encKey will not be destroyed by the garbage
    collector while the mechanism object is still in use. I guess this is
    necessary because the assignment:
    
    self._mech.pParameter = self._encKey
    
    internally copies only a pointer to self._encKey, which would get
    invalidated if one destroys self._encKey and that's why it should be
    attached to self. It wasn't obvious for me either, but this is how it
    works (I guess). The other mechanisms do pretty much the same thing --
    i.e. they store the parameter in self._param usually.
    
    Note:
    .pParameter is just a property of a CK_MECHANISM() object.

--- PyKCS11/__init__.py
+++ PyKCS11/__init__.py
@@ -942,9 +942,11 @@ class CONCATENATE_BASE_AND_KEY_Mechanism:
         """
         :param encKey: a handle of encryption key
         """
+        self._encKey = encKey
+
         self._mech = PyKCS11.LowLevel.CK_MECHANISM()
         self._mech.mechanism = CKM_CONCATENATE_BASE_AND_KEY
-        self._mech.pParameter = encKey
+        self._mech.pParameter = self._encKey
         self._mech.ulParameterLen = PyKCS11.LowLevel.CK_OBJECT_HANDLE_LENGTH
 
     def to_native(self):
