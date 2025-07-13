03a5258721e90801ae07fe8010149f66900fef1e
Ludovic Rousseau <ludovic.rousseau@free.fr>
2025-07-13 15:11:41 +0200

    sample: fix pylint warning
    samples/generate.py:19:0: W0401: Wildcard import PyKCS11 (wildcard-import)
    ²

--- samples/generate.py
+++ samples/generate.py
@@ -16,43 +16,43 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA.
 
-from PyKCS11 import *
+import PyKCS11
 
 # the key_id has to be the same for both objects
 key_id = (0x22,)
 
-pkcs11 = PyKCS11Lib()
+pkcs11 = PyKCS11.PyKCS11Lib()
 pkcs11.load()  # define environment variable PYKCS11LIB=YourPKCS11Lib
 
 # get 1st slot
 slot = pkcs11.getSlotList(tokenPresent=True)[0]
 
-session = pkcs11.openSession(slot, CKF_SERIAL_SESSION | CKF_RW_SESSION)
+session = pkcs11.openSession(slot, PyKCS11.CKF_SERIAL_SESSION | PyKCS11.CKF_RW_SESSION)
 session.login("1234")
 
 pubTemplate = [
-    (CKA_CLASS, CKO_PUBLIC_KEY),
-    (CKA_TOKEN, CK_TRUE),
-    (CKA_PRIVATE, CK_FALSE),
-    (CKA_MODULUS_BITS, 0x0400),
-    (CKA_PUBLIC_EXPONENT, (0x01, 0x00, 0x01)),
-    (CKA_ENCRYPT, CK_TRUE),
-    (CKA_VERIFY, CK_TRUE),
-    (CKA_VERIFY_RECOVER, CK_TRUE),
-    (CKA_WRAP, CK_TRUE),
-    (CKA_LABEL, "My Public Key"),
-    (CKA_ID, key_id),
+    (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
+    (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
+    (PyKCS11.CKA_MODULUS_BITS, 0x0400),
+    (PyKCS11.CKA_PUBLIC_EXPONENT, (0x01, 0x00, 0x01)),
+    (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_VERIFY_RECOVER, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_WRAP, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_LABEL, "My Public Key"),
+    (PyKCS11.CKA_ID, key_id),
 ]
 
 privTemplate = [
-    (CKA_CLASS, CKO_PRIVATE_KEY),
-    (CKA_TOKEN, CK_TRUE),
-    (CKA_PRIVATE, CK_TRUE),
-    (CKA_DECRYPT, CK_TRUE),
-    (CKA_SIGN, CK_TRUE),
-    (CKA_SIGN_RECOVER, CK_TRUE),
-    (CKA_UNWRAP, CK_TRUE),
-    (CKA_ID, key_id),
+    (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
+    (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_PRIVATE, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_DECRYPT, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_SIGN_RECOVER, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_UNWRAP, PyKCS11.CK_TRUE),
+    (PyKCS11.CKA_ID, key_id),
 ]
 
 (pubKey, privKey) = session.generateKeyPair(pubTemplate, privTemplate)
